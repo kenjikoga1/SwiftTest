fa5af6de30349d35dfb3d36345cf8a97a55d7384
kenjikoga1 <43839986+kenjikoga1@users.noreply.github.com>
2019-05-02 12:39:41 +0900

    Start

---
+++ CustomCellSample/CustomCellSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCellSample
+//
+//  Created by 古賀賢司 on 2019/05/02.
+//  Copyright © 2019 古賀賢司. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
